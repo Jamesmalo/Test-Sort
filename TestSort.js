b8321133906e7fd56ffe8625ef9af2b21ea8d774
Jamesmalo <9jmalo@stpatrick.org>
2017-10-05 14:34:54 -0500

    Day 1
    Computer was dying, so I worked with others.

---
+++ TestSort.js
@@ -0,0 +1,10 @@
+const Mod = require('./Sort.js')
+
+function main(){
+    let biglist = [];
+    for(let i=0;i<biglist.length;i++){
+        biglist[x] = Math.random();
+    }
+    let list = 
+}
+main();
